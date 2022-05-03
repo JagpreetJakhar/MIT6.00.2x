7666cb99edc3895a635eddfb573bdb17a1cffcdb
jagpreet <jagpreetjakhar94@gmail.com>
2022-05-03 21:14:27 +0530

    Midterm first Problem

--- Psets/Pset 1/Problem_1.py
+++ Psets/Pset 1/Problem_1.py
@@ -2,6 +2,7 @@ def greedy_cow_transport(cows,limit=10):
     trips = []
     cowsCopy = cows.copy()
     sortedCows=sorted(cowsCopy.items(), key=lambda x:x[1], reverse = True)#sorted and reversed to have weights in descending order
+    print(sortedCows)
     while sum(cowsCopy.values())>0:
         ship =[]
         total =0
@@ -12,7 +13,7 @@ def greedy_cow_transport(cows,limit=10):
                 cowsCopy[i]=0
         trips.append(ship)
     return trips
-  """Test Cases :
+"""Test Cases :
   greedy_cow_transport({'Louis': 45, 'Miss Bella': 15, 'Muscles': 65, 'Patches': 60,\
   'Lotus': 10, 'Polaris': 20, 'MooMoo': 85, 'Clover': 5, 'Horns': 50, 'Milkshake': 75}, 100)
   
@@ -24,3 +25,6 @@ def greedy_cow_transport(cows,limit=10):
   
   
   """
+greedy_cow_transport({'Willow': 35, 'Dottie': 85, 'Patches': 12, 'Daisy': 50, 'Lilly': 24, \
+'Buttercup': 72, 'Betsy': 65, 'Coco': 10, 'Abby': 38, 'Rose': 50}, 100)
+
